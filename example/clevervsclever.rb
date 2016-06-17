d7f3ee81ab089b0a5c48f09665d108a0aeba56f5
Radagast <radagast@openmailbox.org>
2016-06-17 13:12:59 -0300

    Add example

---
+++ example/clevervsclever.rb
@@ -0,0 +1,16 @@
+require 'ruby_cleverbot'
+
+if ARGV[0] != nil
+  bot1 = RubyCleverbot.new
+  bot2 = RubyCleverbot.new
+  seed = ARGV[0]
+  message1 = bot1.send_message(seed)
+  8.times{
+    puts "bot1 : #{message1}"
+    message2 = bot2.send_message(message1)
+    puts "bot2 : #{message2}"
+    message1 = bot1.send_message(message2)
+  }
+else
+  puts "a initial word is required"
+end
